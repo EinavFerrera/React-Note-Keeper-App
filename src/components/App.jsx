adc87fda3ddb8dacb441f7233b305d89eb338d73
EinavFerrera <134479183+EinavFerrera@users.noreply.github.com>
2024-06-15 21:25:05 +0300

    chore: Add bootstrap-icons dependency and fix typo in Heading component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading.jsx";
 import Note from "./Note.jsx";
 import Footer from "./Footer.jsx";
 import note from "../note.js";
+import AddNote from "./AddNote.jsx";
 
 function noteCreate(note) {
   return <Note key={note.key} title={note.title} noteText={note.content} />;
@@ -13,6 +14,7 @@ export default function App() {
     <div>
       <Heading />
       {note.map(noteCreate)}
+      <AddNote />
       <Footer />
     </div>
   );
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Heading() {
   return (
     <header>
-      <h1>Keeper</h1>;
+      <h1>Keeper</h1>
     </header>
   );
 }
